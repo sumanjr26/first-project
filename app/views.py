4383de97fe76dfcd1b552a9cd35171d14ade4e86
sumanjr26 <sumanjrmay26@gmail.com>
2023-03-06 20:42:08 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def details(request):
+    return HttpResponse('<h1><marquee>hello world</h></marquee>')
